56c6fccb4c4a62581964549637b6a4c91726b0b5
Nik Počuča <nikpocuca@gmail.com>
2019-07-17 22:20:23 -0400

    Files generated by PkgTemplates

---
+++ src/MatrixNormTest.jl
@@ -0,0 +1,5 @@
+module MatrixNormTest
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using MatrixNormTest
+using Test
+
+@testset "MatrixNormTest.jl" begin
+    # Write your own tests here.
+end
